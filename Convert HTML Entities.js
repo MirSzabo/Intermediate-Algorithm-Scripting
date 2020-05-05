01d90352b31c3f16a2d4161c18d0903ef9c59cbf
MirSzabo <48127476+MirSzabo@users.noreply.github.com>
2020-05-05 23:53:39 +0200

    Intermediate Algorithm Scripting challenge

---
+++ Convert HTML Entities.js
@@ -0,0 +1,18 @@
+/*Intermediate Algorithm Scripting: Convert HTML Entities
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.*/
+
+function convertHTML(str) {
+  const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  return str
+    .split("")
+    .map(entity => htmlEntities[entity] || entity)
+    .join("");
+}
+
+convertHTML("Dolce & Gabbana");
